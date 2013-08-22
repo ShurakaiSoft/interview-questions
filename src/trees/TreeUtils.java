575fd286e4c9f435453991c66c71c6ff90ffdfd1
Stephen Parker <ShurakaiSoft@gmail.com>
2013-08-22 16:54:22 +1000

    Added some tree utility functions.

---
+++ src/trees/TreeUtils.java
@@ -0,0 +1,70 @@
+package trees;
+
+import java.util.Random;
+
+public class TreeUtils {
+
+	/**
+	 * Creates and fill an array of the requested size with two digit integers.
+	 * 
+	 * @param size of array to create
+	 * @return the array, initialised and populated with random values.
+	 */
+	public static int[] generateArray(int size) {
+		int[] array = new int[size];
+		Random random = new Random();
+		
+		for (int i = 0; i < array.length; i++) {
+			array[i] = random.nextInt(89) + 10;
+		}
+		return array;
+	}
+	
+	
+	
+	
+	/**
+	 * Print the given array as a tree.
+	 * @param array
+	 */
+	public static void print(int[] array) {
+		int i = 0;
+		int rowCount = 1;
+		int indent = array.length;
+		int spaces = 0;
+		while (i < array.length) {
+			
+			indent = (int)Math.floor((float)(indent) / 2.0);
+			for (int k = indent; k > 0; k--) {
+				System.out.print("  ");
+			}
+			
+			for (int j = rowCount; j > 0; j--) {
+				
+				System.out.print(array[i++]);
+				
+				for (int l = spaces; l > 0; l--) {
+					System.out.print("  ");
+				}
+			}
+			
+			spaces = indent;
+			rowCount *= 2;
+			System.out.println();
+		}
+	}
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] array = generateArray(7);
+		
+		print(array);
+
+	}
+
+}
